8851db13a8522a09ccca35b51e045d76cb0a01bd
Luke Horwell <code@horwell.me>
2019-10-03 14:32:37 +0100

    Amendments to packaging scripts.

--- generate-all.sh
+++ generate-all.sh
@@ -2,18 +2,35 @@
 #
 # Generates all the themes - for packaging use only!
 #
+CUR=0
+TOTAL=$(( 10 * 3 ))
 
-CMD="./ubuntu-mate-colours-generator --yes --install-icon-dir=usr/share/icons --install-theme-dir=usr/share/themes --src-dir=/"
+function generate() {
+    theme="$1"
+    hex="$2"
+    name="$3"
+    CUR=$(( CUR + 1 ))
+    echo "Generating $CUR of $TOTAL..."
+    echo "=================================================="
+
+    ./ubuntu-mate-colours-generator --yes \
+        --install-icon-dir=usr/share/icons \
+        --install-theme-dir=usr/share/themes \
+        --src-dir=/ \
+        --theme="$theme" \
+        --hex="$hex" \
+        --name="$name"
+}
 
 for theme in "Ambiant-MATE" "Ambiant-MATE-Dark" "Radiant-MATE"; do
-    $CMD --theme="$theme" --hex="#2DACD4" --name="Aqua"
-    $CMD --theme="$theme" --hex="#5489CF" --name="Blue"
-    $CMD --theme="$theme" --hex="#7F441F" --name="Brown"
-    $CMD --theme="$theme" --hex="#679816" --name="Green"
-    $CMD --theme="$theme" --hex="#E66C1E" --name="Orange"
-    $CMD --theme="$theme" --hex="#E231A3" --name="Pink"
-    $CMD --theme="$theme" --hex="#7E5BC5" --name="Purple"
-    $CMD --theme="$theme" --hex="#CE3A3A" --name="Red"
-    $CMD --theme="$theme" --hex="#1CB39F" --name="Teal"
-    $CMD --theme="$theme" --hex="#D8A200" --name="Yellow"
+    generate "$theme" "#2DACD4" "Aqua"
+    generate "$theme" "#5489CF" "Blue"
+    generate "$theme" "#7F441F" "Brown"
+    generate "$theme" "#679816" "Green"
+    generate "$theme" "#E66C1E" "Orange"
+    generate "$theme" "#E231A3" "Pink"
+    generate "$theme" "#7E5BC5" "Purple"
+    generate "$theme" "#CE3A3A" "Red"
+    generate "$theme" "#1CB39F" "Teal"
+    generate "$theme" "#D8A200" "Yellow"
 done
