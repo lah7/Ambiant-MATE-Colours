642377bafa067e71fd58bbb0f150f6f3e1ba8a93
Luke Horwell <code@horwell.me>
2020-04-26 20:57:42 +0100

    CI: Lint Python file for errors

---
+++ scripts/lint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Performs a lint check on the Python generator script.
+#
+# Requires pylint for Python 3 to be installed.
+#   In Ubuntu that package is "pylint3".
+#   Some distro/releases might use "pylint".
+#
+
+pylint=""
+
+if [ ! -z "$(which pylint3 2>/dev/null)" ]; then
+    pylint="pylint3"
+elif [ ! -z "$(which pylint 2>/dev/null)" ]; then
+    pylint="pylint"
+fi
+
+if [ -z "$pylint" ]; then
+    echo "Please install 'pylint3' to use this script."
+    exit 1
+fi
+
+$pylint ubuntu-mate-colours-generator --errors-only
+echo "Lint returned exit code $?"
